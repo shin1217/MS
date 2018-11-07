113f9322411a16b36ced11ab158d9f361b0465cc
SHIN <SHIN@SHIN>
2018-11-07 10:26:34 +0900

    asdf

---
+++ MS/src/main/java/com/bit/ms/admin/controller/NoticeDeleteController.java
@@ -0,0 +1,25 @@
+package com.bit.ms.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitcamp.pc.admin.service.NoticeDeleteService;
+
+@Controller
+public class NoticeDeleteController {
+
+	@Autowired
+	private NoticeDeleteService service;
+
+	// 삭제 모달창에서 삭제를 누르면 해당 글이 삭제되고 목록으로 돌아감
+	@RequestMapping(value = "/admin/notice/delete/{id}", method = RequestMethod.GET)
+	public String noticeDelete(@PathVariable("id") int id) {
+
+		service.noticeDelete(id);
+		
+		return "redirect:/member/notice?page=1";
+	}
+}
