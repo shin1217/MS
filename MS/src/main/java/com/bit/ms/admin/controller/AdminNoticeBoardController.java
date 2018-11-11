a681a1453549750cab590667f82eb2a0708123e6
SHIN <SHIN@SHIN>
2018-11-11 19:43:29 +0900

    공지사항 컨트롤러 통합

--- MS/src/main/java/com/bit/ms/admin/controller/AdminNoticeBoardController.java
+++ MS/src/main/java/com/bit/ms/admin/controller/AdminNoticeBoardController.java
@@ -13,15 +13,32 @@ import com.bit.ms.admin.service.AdminNoticeBoardService;
 import com.bit.ms.member.service.MemberNoticeViewService;
 
 @Controller
-@RequestMapping("/admin/notice/modify/{id}")
-public class AdminNoticeModifyController {
+@RequestMapping("/admin")
+public class AdminNoticeBoardController {
 
 	@Autowired
 	MemberNoticeViewService viewService;
 	@Autowired
-	AdminNoticeBoardService modifyService;
+	AdminNoticeBoardService noticeBoardService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	// 공지사항에서 글쓰기 클릭시 글쓰기폼으로 이동
+	@RequestMapping(value = "/noticeWrite", method = RequestMethod.GET)
+	public String getWriteForm() {
+
+		return "admin/adminNoticeWriteForm";
+	}
+
+	// 글작성 완료시 다시 공지사항 게시판으로 이동
+	@RequestMapping(value = "/noticeWrite", method = RequestMethod.POST)
+	public String writeSuccess(NoticeVO noticeVO) {
+
+		noticeBoardService.noticeWrite(noticeVO);
+
+		return "redirect:/member/notice?page=1";
+	}
+
+	// 글 수정
+	@RequestMapping(value = "/notice/modify/{id}", method = RequestMethod.GET)
 	public ModelAndView noticeModify(@PathVariable("id") int id) {
 
 		ModelAndView modelAndView = new ModelAndView();
@@ -33,13 +50,23 @@ public class AdminNoticeModifyController {
 		return modelAndView;
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	// 글 수정
+	@RequestMapping(value = "/notice/modify/{id}", method = RequestMethod.POST)
 	public String noticeModifySuccess(@PathVariable("id") int id, NoticeVO noticeVO, Model model) {
 
 		noticeVO.setNotice_id(id); // 받은 id값을 notice_id에 넣어줌
 
-		modifyService.noticeModify(noticeVO);
+		noticeBoardService.noticeModify(noticeVO);
 
 		return "redirect:/member/notice/" + id;
+	}
+
+	// 삭제 모달창에서 삭제를 누르면 해당 글이 삭제되고 목록으로 돌아감
+	@RequestMapping(value = "/notice/delete/{id}", method = RequestMethod.GET)
+	public String noticeDelete(@PathVariable("id") int id) {
+
+		noticeBoardService.noticeDelete(id);
+
+		return "redirect:/member/notice?page=1";
 	}
 }
