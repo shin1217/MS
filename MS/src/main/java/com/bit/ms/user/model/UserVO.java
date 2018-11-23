898a915a2393b117298feb963094909eea9807de
1027 <1027@hb3007>
2018-11-23 10:17:15 +0900

    2018.11.23 10:16 userVO수정 store_id 재추가 by JK

--- MS/src/main/java/com/bit/ms/user/model/UserVO.java
+++ MS/src/main/java/com/bit/ms/user/model/UserVO.java
@@ -14,75 +14,126 @@ public class UserVO {
 	private String user_birth; // 생년월일
 	private String user_email; // 이메일
 	private long user_time; // 남은 시간
+	private int store_id; // 사용 중인 매장 번호
 	private int seat_id; // 사용 중인 좌석 번호
 	private int use_pay; // 결제 금액
 	private String user_key; // 인증 번호
-	public String getUser_id() {
-		return user_id;
+
+	public UserVO() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public void setUser_id(String user_id) {
+
+	public UserVO(String user_id, String user_name, String user_pw, String user_phone, String user_birth,
+			String user_email, long user_time, int store_id, int seat_id, int use_pay, String user_key) {
+		super();
 		this.user_id = user_id;
-	}
-	public String getUser_name() {
-		return user_name;
-	}
-	public void setUser_name(String user_name) {
 		this.user_name = user_name;
-	}
-	public String getUser_pw() {
-		return user_pw;
-	}
-	public void setUser_pw(String user_pw) {
 		this.user_pw = user_pw;
-	}
-	public String getUser_phone() {
-		return user_phone;
-	}
-	public void setUser_phone(String user_phone) {
 		this.user_phone = user_phone;
-	}
-	public String getUser_birth() {
-		return user_birth;
-	}
-	public void setUser_birth(String user_birth) {
 		this.user_birth = user_birth;
-	}
-	public String getUser_email() {
-		return user_email;
-	}
-	public void setUser_email(String user_email) {
 		this.user_email = user_email;
-	}
-	public long getUser_time() {
-		return user_time;
-	}
-	public void setUser_time(long user_time) {
 		this.user_time = user_time;
-	}
-	public int getSeat_id() {
-		return seat_id;
-	}
-	public void setSeat_id(int seat_id) {
+		this.store_id = store_id;
 		this.seat_id = seat_id;
-	}
-	public int getUse_pay() {
-		return use_pay;
-	}
-	public void setUse_pay(int use_pay) {
 		this.use_pay = use_pay;
+		this.user_key = user_key;
 	}
-	public String getUser_key() {
-		return user_key;
-	}
-	public void setUser_key(String reg_key) {
-		this.user_key = reg_key;
-	}
+
 	@Override
 	public String toString() {
 		return "UserVO [user_id=" + user_id + ", user_name=" + user_name + ", user_pw=" + user_pw + ", user_phone="
 				+ user_phone + ", user_birth=" + user_birth + ", user_email=" + user_email + ", user_time=" + user_time
-				+ ", seat_id=" + seat_id + ", use_pay=" + use_pay + ", user_key=" + user_key + "]";
+				+ ", store_id=" + store_id + ", seat_id=" + seat_id + ", use_pay=" + use_pay + ", user_key=" + user_key
+				+ "]";
 	}
-	
-	
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_pw() {
+		return user_pw;
+	}
+
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+
+	public String getUser_phone() {
+		return user_phone;
+	}
+
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+
+	public String getUser_birth() {
+		return user_birth;
+	}
+
+	public void setUser_birth(String user_birth) {
+		this.user_birth = user_birth;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public long getUser_time() {
+		return user_time;
+	}
+
+	public void setUser_time(long user_time) {
+		this.user_time = user_time;
+	}
+
+	public int getStore_id() {
+		return store_id;
+	}
+
+	public void setStore_id(int store_id) {
+		this.store_id = store_id;
+	}
+
+	public int getSeat_id() {
+		return seat_id;
+	}
+
+	public void setSeat_id(int seat_id) {
+		this.seat_id = seat_id;
+	}
+
+	public int getUse_pay() {
+		return use_pay;
+	}
+
+	public void setUse_pay(int use_pay) {
+		this.use_pay = use_pay;
+	}
+
+	public String getUser_key() {
+		return user_key;
+	}
+
+	public void setUser_key(String user_key) {
+		this.user_key = user_key;
+	}
+
 }
