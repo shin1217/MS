bd4dc8658ceea721966e2b6e9395ac3f733b5ee0
kim min su <mkms1104@naver.com>
2018-11-28 09:50:32 +0900

    fsd

--- MS/src/main/java/com/bit/ms/interceptor/UsingSeatCheckInterceptor.java
+++ MS/src/main/java/com/bit/ms/interceptor/UsingSeatCheckInterceptor.java
@@ -1,7 +1,5 @@
 package com.bit.ms.interceptor;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,7 +7,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.bit.ms.user.model.UserVO;
 import com.bit.ms.user.service.UserTimeService;
 
 public class UsingSeatCheckInterceptor extends HandlerInterceptorAdapter {
@@ -27,17 +24,7 @@ public class UsingSeatCheckInterceptor extends HandlerInterceptorAdapter {
 
 		if (session != null) {
 			
-			UserVO userVo = (UserVO)session.getAttribute("userSession");
-			String sessionId = userVo.getUser_id();
 			
-			List<UserVO> usingList = service.getUserInfoAll(userVo.getStore_id());
-			for(int i=0; i<usingList.size(); i++) {
-				String usingId = usingList.get(i).getUser_id();
-				
-				if(sessionId.equals(usingId)) { // 현재 접속한 아이디의 좌석 사용 여부 검사
-					return true;
-				}
-			}
 		}
 		response.sendRedirect(request.getContextPath() + "/user/main"); // 좌석 미 등록 시 메인페이지로 이동
 		return false;
